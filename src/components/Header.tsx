59d5296aa9bb965d6b9885dbcc8dd9b5fda30060
dodrin <indrn@icloud.com>
2025-02-02 13:12:08 -0800

    Implement product grid

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,15 @@ import Link from "next/link";
 import React from "react";
 import { ButtonLink } from "./ButtonLink";
 import { Logo } from "./Logo";
+import { createClient } from "@/prismicio";
+import { PrismicNextLink } from "@prismicio/next";
 
 type Props = {};
 
-export function Header({}: Props) {
+export async function Header({}: Props) {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
   return (
     <header className="header absolute left-0 right-0 top-0 z-50 ~h-32/48 ~px-4/6 ~py-4/6 hd:h-32">
       <div className="mx-auto grid w-full max-w-6xl grid-cols-[auto,auto] items-center gap-6 md:grid-cols-[1fr,auto,1fr]">
@@ -17,8 +22,11 @@ export function Header({}: Props) {
           className="col-span-full row-start-2 md:col-span-1 md:col-start-2 md:row-start-1"
         >
           <ul className="flex flex-wrap items-center gap-8">
-            <li>Boards</li>
-            <li>Boards</li>
+            {settings.data.navigation.map((item) => (
+              <li key={item.link.text}>
+                <PrismicNextLink field={item.link} className="~text-lg/xl" />
+              </li>
+            ))}
           </ul>
         </nav>
         <div className="justify-self-end">
